4c9e7bf7c0ec864a2d7f28c25b2c53b4353458e8
Wayne <eunchan0817@gmail.com>
2023-06-12 20:24:55 +0900

    Initial Commit

---
+++ Practice-Objective-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Practice-Objective-C
+//
+//  Created by Wayne Kim on 2023/06/12.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
